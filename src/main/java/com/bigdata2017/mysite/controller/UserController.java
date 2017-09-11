115cae8be6adc8a57bf9167a24f367e64efe59e5
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-09-11 17:01:34 +0900

    수정1

--- src/main/java/com/bigdata2017/mysite/controller/UserController.java
+++ src/main/java/com/bigdata2017/mysite/controller/UserController.java
@@ -4,13 +4,11 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.bigdata2017.mysite.exception.UserDaoException;
 import com.bigdata2017.mysite.service.UserService;
 import com.bigdata2017.mysite.vo.UserVo;
 
--- src/main/java/com/bigdata2017/mysite/repository/UserDao.java
+++ src/main/java/com/bigdata2017/mysite/repository/UserDao.java
@@ -1,9 +1,4 @@
 package com.bigdata2017.mysite.repository;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
