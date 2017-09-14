3ad30926ee11be9fb6ba97094cebf48dc5cdd2e3
bit-user <bit-user@DESKTOP-C1QR4BV>
2017-09-14 13:27:45 +0900

    admin page - 1

--- src/main/java/com/bigdata2017/mysite/controller/AdminController.java
+++ src/main/java/com/bigdata2017/mysite/controller/AdminController.java
@@ -3,11 +3,22 @@ package com.bigdata2017.mysite.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.bigdata2017.security.Auth;
-
-
 @Controller
-@Auth(role="admin")
 @RequestMapping( "/admin" )
 public class AdminController {
+
+	@RequestMapping({"", "/main"})
+	public String main() {
+		return "admin/main";
+	}
+	
+	@RequestMapping("/guestbook")
+	public String guestbook() {
+		return "admin/guestbook";
+	}
+	
+	@RequestMapping("/board")
+	public String board() {
+		return "admin/board";
+	}	
 }
